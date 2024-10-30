6f7965ae68567d7d7ddcfa6b3d3537cd58b8ca41
Seriy87 <56091911+Seriy87@users.noreply.github.com>
2024-10-30 19:53:08 +0300

    Delete BotBybit directory

--- BotBybit/strategy.py
+++
@@ -1,9 +0,0 @@
-from utils import calculate_take_profit_and_stop_loss
-
-def should_place_order(last_price, high_level, low_level, side):
-    """Проверка условий для размещения ордера на покупку или продажу."""
-    if side == 'sell' and last_price >= high_level:
-        return 'sell'
-    elif side == 'buy' and last_price <= low_level:
-        return 'buy'
-    return None
